72566d7ac2dca0894c04ca0b83ae5c4cccb551e1
bik <ykaifei@163.com>
2021-08-22 19:25:11 +0800

    common

---
+++ src/main/java/pers/bik/es/common/EsMessage.java
@@ -0,0 +1,24 @@
+package pers.bik.es.common;
+
+/**
+ * ES的相关信息
+ * @author yangkaifei
+ * @date 2021/8/22 6:31 下午
+ * @cersion 1.0
+ */
+public class EsMessage {
+    /**
+     * 链接地址
+     */
+    public static final String ADDRESS = "localhost";
+
+    /**
+     * 端口
+     */
+    public static final int PORT = 9200;
+    /**
+     * 访问方式
+     */
+    public static final String SCHEME = "http";
+
+}
